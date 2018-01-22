892e16482733794a3d1c1b9f5cd4c2ed3d996e13
Antti Virkkunen <antti.virkkunen@gmail.com>
2018-01-22 17:10:37 +0200

    Additional comments

--- TehtTankki/Tank.cs
+++ TehtTankki/Tank.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace TehtTankki
 {
+    /// <summary>
+    /// An armored fighting vehicle
+    /// </summary>
     class Tank
     {
         public String Name { get; set; }
@@ -13,6 +16,10 @@ namespace TehtTankki
         private float SpeedMax = 100;
 
         private int crewCount;
+        /// <summary>
+        /// The number of crew on board the tank
+        /// Must be between 2 and 6
+        /// </summary>
         public int CrewCount
         {
             get
@@ -29,6 +36,11 @@ namespace TehtTankki
         }
         public float Speed { get; private set; }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name">The name of the tank</param>
+        /// <param name="type">The type of the tank</param>
         public Tank(String name, String type)
         {
             this.Name = name;
@@ -37,6 +49,9 @@ namespace TehtTankki
             this.Speed = 0;
         }
 
+        /// <summary>
+        /// Default constructor, both name and type set to "default tank"
+        /// </summary>
         public Tank() : this("generic tank", "generic tank")
         {
         }
