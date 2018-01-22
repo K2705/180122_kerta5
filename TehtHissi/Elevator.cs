8cdcd88d148d56045aa394fe08a01bffec7b45b8
Antti Virkkunen <antti.virkkunen@gmail.com>
2018-01-22 17:10:48 +0200

    Elevator, first pass

---
+++ TehtHissi/Elevator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TehtHissi
+{
+    class Elevator
+    {
+        public readonly int MinFloor = 1;
+        public readonly int MaxFloor = 5;
+        private int floor;
+        public int Floor
+        {
+            get { return floor; }
+            set
+            {
+                if (value >= MinFloor && value <= MaxFloor)
+                {
+                    floor = value;
+                }
+            }
+        }
+
+        public Elevator(int minFloor, int maxFloor)
+        {
+            this.MinFloor = minFloor;
+            this.MaxFloor = maxFloor;
+            this.floor = minFloor;
+        }
+
+    }
+}
